ca3d12c3c041f05e7e784649e9ca9fc1df27d607
Dhruvlad04 <aapu49260@gmail.com>
2026-03-31 20:57:15 +0530

    Create Task.py

---
+++ Task.py
@@ -0,0 +1,9 @@
+#python program to create a simple calculator
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number:"))
+
+print("addition:", num1 + num2)
+print("subtraction:",num1 - num2)
+print("multiplication:",num1 * num2)
+print("division:",num1 / num2)
