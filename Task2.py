20b109bda448d4f1ab9cee1963b650bea5637cd3
Dhruvlad04 <aapu49260@gmail.com>
2026-03-31 21:08:15 +0530

    Add personalized greeting program in Task2.py

---
+++ Task2.py
@@ -0,0 +1,6 @@
+# Pyjton program to create  a personalized  greeting
+
+first_name = input("Enter youre first name : ")
+last_name =input("Enter youre last name : ")
+
+print(" Hello ,"+ first_name+ " " + last_name +" ! Welcome to python programming")
